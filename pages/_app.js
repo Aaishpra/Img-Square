56bc0a49c8db001341eba006d9f336d232190122
K-Kumar01 <kushalkumargupta4@gmail.com>
2020-10-30 21:01:17 +0530

    Added basic auth page

---
+++ pages/_app.js
@@ -0,0 +1,28 @@
+import { motion } from 'framer-motion';
+
+import '../styles/globals.css';
+
+export default function App({ Component, pageProps, router }) {
+	return (
+		<motion.div
+			key={router.route}
+			initial="hidden"
+			animate="visible"
+			variants={{
+				hidden: {
+					scale: 0.8,
+					opacity: 0,
+				},
+				visible: {
+					scale: 1,
+					opacity: 1,
+					transition: {
+						delay: 0.4,
+					},
+				},
+			}}
+		>
+			<Component {...pageProps} />
+		</motion.div>
+	);
+}
---
+++ pages/authenticate.js
@@ -0,0 +1,12 @@
+import React from "react";
+import Authenitcate from "../components/authenticate/Authenticate";
+
+const AuthenticatePage = () => {
+  return (
+    <div className="authenticate">
+      <Authenitcate />
+    </div>
+  );
+};
+
+export default AuthenticatePage;
